33422fa9347586e2c0876a87d19775c278309896
Kirill <krikus.ms@gmail.com>
2022-03-07 22:20:58 +0100

    minor changes

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+Paclet[
+    Name -> "JTP",
+    Version -> "0.1",
+    MathematicaVersion -> "10+",
+    Description -> "SMS Gateway with Wolfram Mathematica",
+    Creator -> "Kirill Vasin & Kirill Belov",
+    Extensions -> 
+        {
+            {"Kernel", Root -> ".", Context -> "JTP`"}
+        }
+]
